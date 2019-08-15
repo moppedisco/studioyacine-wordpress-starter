6d16e7c85dd13fb209f6ab1a168499c152332420
Yacine Belarbi <yacbel@gmail.com>
2019-08-15 16:48:31 +0200

    markup fixes

--- wp-content/themes/studioyacine/taxonomy-custom_cat.php
+++ wp-content/themes/studioyacine/taxonomy-custom_cat.php
@@ -14,64 +14,51 @@
 
 <?php get_header(); ?>
 
-			<div id="content">
+<main id="main" class="" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 
-				<div id="inner-content" class="wrap cf">
+	<h1 class="archive-title"><span><?php _e( 'Posts Categorized:', 'bonestheme' ); ?></span> <?php single_cat_title(); ?></h1>
 
-						<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-							<h1 class="archive-title h2"><span><?php _e( 'Posts Categorized:', 'bonestheme' ); ?></span> <?php single_cat_title(); ?></h1>
+	<article id="post-<?php the_ID(); ?>" <?php post_class( '' ); ?>>
 
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		<header class="article-header">
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
+			<h3><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
 
-								<header class="article-header">
+		</header>
 
-									<h3 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-									<p class="byline vcard"><?php
-										printf(__('Posted <time class="updated" datetime="%1$s" itemprop="datePublished">%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', 'bonestheme'), get_the_time('Y-m-j'), get_the_time(__('F jS, Y', 'bonestheme')), bones_get_the_author_posts_link(), get_the_term_list( get_the_ID(), 'custom_cat', "", ", ", "" ));
-									?></p>
+		<section class="entry-content">
+			<?php the_excerpt( '<span class="read-more">' . __( 'Read More &raquo;', 'bonestheme' ) . '</span>' ); ?>
 
-								</header>
+		</section>
 
-								<section class="entry-content">
-									<?php the_excerpt( '<span class="read-more">' . __( 'Read More &raquo;', 'bonestheme' ) . '</span>' ); ?>
+		<footer class="article-footer">
 
-								</section>
+		</footer>
 
-								<footer class="article-footer">
+	</article>
 
-								</footer>
+	<?php endwhile; ?>
 
-							</article>
+			<?php bones_page_navi(); ?>
 
-							<?php endwhile; ?>
+	<?php else : ?>
 
-									<?php bones_page_navi(); ?>
+			<article id="post-not-found" class="hentry ">
+				<header class="article-header">
+					<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+				</header>
+				<section class="entry-content">
+					<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+				</section>
+				<footer class="article-footer">
+						<p><?php _e( 'This is the error message in the taxonomy-custom_cat.php template.', 'bonestheme' ); ?></p>
+				</footer>
+			</article>
 
-							<?php else : ?>
+	<?php endif; ?>
 
-									<article id="post-not-found" class="hentry cf">
-										<header class="article-header">
-											<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-										</header>
-										<section class="entry-content">
-											<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
-										</section>
-										<footer class="article-footer">
-												<p><?php _e( 'This is the error message in the taxonomy-custom_cat.php template.', 'bonestheme' ); ?></p>
-										</footer>
-									</article>
-
-							<?php endif; ?>
-
-						</main>
-
-						<?php get_sidebar(); ?>
-
-				</div>
-
-			</div>
+</main>
 
 <?php get_footer(); ?>
